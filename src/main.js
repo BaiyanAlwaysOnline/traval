7a369784889961d1fd2f933a082125ded5508800
Your Name <675638513@qq.com>
2019-06-15 19:12:37 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '@/assets/styles/border.css'
+import '@/assets/styles/reset.css'
+import fastClick from 'fastclick'
 
+// 阻止启动生产消息，常用作指令。
 Vue.config.productionTip = false
 
+fastClick.attach(document.body)
+
 /* eslint-disable no-new */
+console.log(App)
 new Vue({
   el: '#app',
   router,
