b1e430e2c65591ef3a1d5625a635b37f2bf9e232
Your Name <675638513@qq.com>
2019-06-22 23:19:01 +0800

    finish header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
+    <header></header>
     <banner></banner>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import Banner from './components/Banner'
+import Header from './components/Header'
 
 export default {
   name: 'Detail',
@@ -15,9 +18,13 @@ export default {
     }
   },
   components: {
-    Banner
+    Banner,
+    Header
   }
 }
 </script>
 
-<style lang=""></style>
+<style lang="stylus" scoped>
+  .content
+    height 50 rem
+</style>
